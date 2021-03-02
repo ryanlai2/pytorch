4458f893ef8cd445f1dcdb3053a821b17f759aa2
Meghan Lele <meghanl@fb.com>
2021-03-02 15:30:34 -0800

    [JIT] Add reference semantics to TorchScript classes
    **Summary**
    This commit adds reference semantics to TorchScript class types;
    modifications made to them within TorchScript will be visible in Python.
    
    **Test Plan**
    This commit adds a unit test to `TestClassType` that checks that
    modifications made to a class type instance passed into TorchScript are
    visible in Python after executing the scripted function or module.
    
    **Fixes**
    This commit closes #41421.
    
    ghstack-source-id: fdc533f88fa15c4a37958203f34a1db0b964427c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44324

--- torch/csrc/jit/python/pybind_utils.cpp
+++ torch/csrc/jit/python/pybind_utils.cpp
@@ -152,10 +152,26 @@ IValue toIValue(py::handle obj, const TypePtr& type, c10::optional<int32_t> N) {
     }
     case TypeKind::ClassType: {
       auto classType = type->expect<ClassType>();
-      if (auto mod = as_module(py::cast<py::object>(obj))) {
+      auto object = py::cast<py::object>(obj);
+      if (auto mod = as_module(object)) {
         // if obj is already a ScriptModule, just return its ivalue
         return mod.value()._ivalue();
       }
+
+      // Check if the obj is a ScriptClass.
+      if (py::isinstance(
+              obj,
+              py::module::import("torch.jit").attr("RecursiveScriptClass"))) {
+        auto inst = py::cast<Object>(obj.attr("_c"));
+        return inst._ivalue();
+      }
+
+      try {
+        Object* script_object = object.cast<Object*>();
+        return script_object->_ivalue();
+      } catch (...) {
+      }
+
       // otherwise is a normal class object, we create a fresh
       // ivalue::Object to use from the py object.
       // 1. create a bare ivalue
@@ -203,6 +219,9 @@ IValue toIValue(py::handle obj, const TypePtr& type, c10::optional<int32_t> N) {
       if (auto mod = as_module(py::cast<py::object>(obj))) {
         classType = mod.value().type();
         res = mod.value()._ivalue();
+      } else if (auto object = as_object(py::cast<py::object>(obj))) {
+        classType = object.value().type();
+        res = object.value()._ivalue();
       } else {
         // We inspect the value to found the compiled TorchScript class
         // and then create a ivalue::Object from that class type.
@@ -224,8 +243,8 @@ IValue toIValue(py::handle obj, const TypePtr& type, c10::optional<int32_t> N) {
       std::stringstream why_not;
       if (!classType->isSubtypeOfExt(interfaceType, &why_not)) {
         throw py::cast_error(c10::str(
-            "Object ",
-            py::str(obj),
+            "Object of type ",
+            classType->repr_str(),
             " is not compatible with interface ",
             interfaceType->repr_str(),
             "\n",
